5448c8c6799dcad0b28ecac5b5a91ff93f02363f
Sergey <Serg_Sh_@mail.ru>
2019-01-31 10:31:19 +0300

    Patterns System out

---
+++ System_Out.java
@@ -0,0 +1,13 @@
+package Task5_2;
+
+public class System_Out {
+	public static void main (String[]args) {
+		Patterns patterns = new Patterns();
+		System.out.println(patterns.gettest("they"));
+		System.out.println(patterns.gettest("jins"));
+		System.out.println(patterns.gettest2("12OP"));
+		System.out.println(patterns.gettest2("12OP "));
+		
+	}
+
+}
